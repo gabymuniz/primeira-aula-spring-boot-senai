52216637265d21b3349d595bbd99e49274ce6213
Joao <joao.m.junior7@docente.senai.br>
2026-08-03 19:17:06 -0300

    Anotações na classe

--- src/main/java/com/example/primeiraaulaspringboot/controller/PessoaController.java
+++ src/main/java/com/example/primeiraaulaspringboot/controller/PessoaController.java
@@ -5,20 +5,31 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
-@RestController
-@RequestMapping("/pessoa")
+// Essa classe Controller é responsavel por fazer requisições web e devolver respostas
+//Por padrão, essas respostas são em formato de texto JSON
+
+/**
+ * Requisições HTTP:
+ *  GET -> Ler -> Solicita dados específicos no servidor.
+ *  POST -> Criar -> Envia dados para o servidor.
+ *  PUT -> Subsitui -> Atualiza algum dado no meu servidor
+ *  DELETE -> Apaga -> Remove algum recurso do servidor.
+ */
+
+@RestController // Anotação que permite esta classe fazer requisições HTTP
+@RequestMapping("/pessoa") // Toda vez que for acessar um metodo da classe PessoaControler, precia colocar um /pessoa antes de chamar seu outro Mapping
 public class PessoaController {
     //lista que vai guardar todos os nossos objetos da classe PessoaModel
     private final ArrayList<PessoaModel> pessoas = new ArrayList<>();
 
     //localhost:8080/pessoa/listaPessoas
-    @GetMapping("/listaPessoas")
+    @GetMapping("/listaPessoas") // Faz requisições GET, nesse caso, mostrando os valores dentro da lista pessoas
     public ArrayList<PessoaModel> listaPessoas(){
         return pessoas;
     }
 
     //localhost:8080/pessoa/adicionarPessoa
-    @PostMapping("/adicionarPessoa")
+    @PostMapping("/adicionarPessoa") // Envia uma requisição POST, adicionando um novo valor dentro da lista pessoas
     public PessoaModel adicionarPessoa(@RequestBody PessoaModel novaPessoa){
         pessoas.add(novaPessoa);
         return novaPessoa;
